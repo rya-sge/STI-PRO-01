de2911f54857bf2851187077b2dc1ffff2fbe1d3
Ryan Sauge <ryan.sauge@heig-vd.ch>
2021-09-29 18:37:10 +0200

    Modiifer script pour ajouter pwd + ajout script pour supprimer container

---
+++ site/delete.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+sudo docker kill  sti_project
+sudo docker system prune
--- site/script.sh
+++ site/script.sh
@@ -1,5 +1,7 @@
 #/bin/bash
-docker run -ti -v "site":/usr/share/nginx/ -d -p 8080:80 --name sti_project --hostname sti arubinst/sti:project2018
+docker run -ti -v "$PWD/site":/usr/share/nginx/ -d -p 8080:80 --name sti_project --hostname sti arubinst/sti:project2018
+
+
 
 docker exec -u root sti_project service nginx start
 
