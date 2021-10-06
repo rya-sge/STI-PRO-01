cf446e8a7d552ce7f678c9eaf540528cd87fd64a
Tchewi Atchoum <tchewiatchoum@gmail.com>
2021-10-06 17:50:33 +0200

    Correction MYSQL - SQLight

---
+++ ScriptsSQL/databaseSQLight.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Role (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL
+);
+
+CREATE TABLE User (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL,
+	password TEXT,
+	email TEXT,
+	isValid boolean,
+	roleId INTEGER,
+	FOREIGN KEY(roleId) REFERENCES Role(id)
+);
+
+CREATE TABLE Message (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL,
+	dateReceipt TEXT,
+	sender INTEGER,
+	recipient INTEGER,
+	subject Text,
+	body Text
+);
+-- https://www.sqlite.org/foreignkeys.html
